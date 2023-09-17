4e965cca02ac8351a432d555c1d6da9e9b88d16e
Brli126 <zy972822@gmail.com>
2023-09-17 18:45:48 -0400

    Add the logic for players to set mark on the webpage.

--- tictactoc.js
+++ tictactoc.js
@@ -1,30 +1,86 @@
 
 
 const gameBoard = (() => {
-    const gameBoardArr = ["X", "X", "O", "X", "O", "O", "X", "O", "X"];
+    const gameBoardArr = ["", "", "", "", "", "", "", "", ""];
     const ncell = 9; // number of cells.
     
+    // Render the array to a board on webpage
     const render = function() {
         for (let i = 0; i < ncell; i++) {
             const cell = document.querySelector(`#cell_${i} p`);
-            if (gameBoardArr[i] == "X") {
+            if (gameBoardArr[i] === "X") {
                 cell.textContent = "X";
-            } else {
+            } else if (gameBoardArr[i] === "O"){
                 cell.textContent = "O";
-            }
+            } else continue;
         }
-    }
+    };
 
-    return {render};
+    const updateArr = function(idx, mark) {
+            gameBoardArr[idx] = mark;
+    };
+
+    return {render, updateArr};
 
 })()
 
-gameBoard.render();
 
-// Factory function to create player object.
-function player() {
+const cells = document.querySelectorAll('.cell');
 
+// Factory functon to create a game.
+
+const game = () => {
+    // Mode: pvp or pve
+    const Mode = "pvp";
+
+    
+    const player1 = player("X");
+    const player2 = player("O");
+    let currentPlayer = player1;
+
+    const getCellIndex = function(cell) {
+        return Number(cell.getAttribute('data-index'));
+    }
+ 
+    const takeTurn = function() {
+        if (currentPlayer === player1) {
+            currentPlayer = player2;
+        } else {
+            currentPlayer = player1;
+        }
+    }
+
+
+
+    const start = function() {
+        
+        cells.forEach(cell => cell.addEventListener('click', 
+            () => {currentPlayer.setMark(getCellIndex(cell));   // place piece
+                   takeTurn();                                  // upadate the current player
+                   gameBoard.render();                          // display
+                },  
+                   {once: true}));                              // Can only click once per cell.
+            
+        
+    };
+
+    return{start};
 }
 
 
 
+
+
+
+// Factory function to create player object.
+const player = (myMark) => {
+    const mark = myMark;
+    const setMark = function(idx) {
+        gameBoard.updateArr(idx, mark);
+    };
+
+    return {setMark};
+}
+
+const newgame = game();
+newgame.start();
